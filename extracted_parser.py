797330e211b6572177c6d7e5362941b4339858ea
Rav <ravishrandev@gmail.com>
2025-06-15 14:08:06 +1000

    Update extracted_parser.py
    added dictionary and appending record data into it.
    
    removed printing functionality to make code more modular

--- extracted_parser.py
+++ extracted_parser.py
@@ -2,22 +2,31 @@ import os
 import xml.etree.ElementTree as ET
 
 def xml_parser(source_dir='extracted'):
+
+    records = []                                                #store records in dictionary
+
     if not os.path.exists(source_dir):
         print("❌ Oh oh, looks like source destination does not exist")
-        return
+        return records
     
     for filename in os.listdir(source_dir):                     #go through all files in the folder
         if filename.endswith('.xml'):                          
             xml_path = os.path.join(source_dir, filename)       #create file path for future use
-            with open(xml_path, 'r') as f:
-                print(f"📂 Reading: {filename}")
-                tree = ET.parse(xml_path)
-                root = tree.getroot() 
-                #print(ET.tostring(root, encoding='unicode'))
-                for record in root.findall('record'):
-                    row = record.find('row')
-                    source_ip = row.find('source_ip').text
-                    count = row.find('count').text
+            print(f"📂 Reading: {filename}")
+            tree = ET.parse(xml_path)
+            root = tree.getroot() 
+            #print(ET.tostring(root, encoding='unicode'))
 
-                    print("📌 Source IP:", source_ip)
-                    print("📊 Count:", count)
+            for record in root.findall('record'):
+                row = record.find('row')
+                policy = row.find('policy_evaluated')
+
+                records.append({
+                    'ip': row.find('source_ip').text,
+                    'count': row.find('count').text,
+                    'spf': policy.find('spf').text,
+                    'dkim': policy.find('dkim').text,
+                    'disposition' : policy.find('disposition').text
+                })
+
+    return records
