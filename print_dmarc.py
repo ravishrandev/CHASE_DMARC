76cd0a2a95253a5ea5ff3fdef293b9378c77ecad
Rav <ravishrandev@gmail.com>
2025-06-15 14:46:36 +1000

    Update print_dmarc.py
    include other fields like envelopes etc...

--- print_dmarc.py
+++ print_dmarc.py
@@ -2,12 +2,28 @@ from tabulate import tabulate
 
 def print_dmarc_table(records):
     if not records:
-        print ("⚠️ No DMARC records to display.")
+        print("⚠️ No DMARC records to display.")
         return
-    
-    headers = ["IP Address", "Email Volume", "SPF", "DKIM", "DMARC"]
-    row = [[r['ip'], r['count'], r['spf'], r['disposition']] for r in records]
+
+    headers = [
+        "IP Address", "Email Volume", "Envelope To", "Envelope From",
+        "Header From", "DKIM Domain", "DKIM Result", "SPF Result", "DMARC Disposition"
+    ]
+
+    rows = [
+        [
+            r['ip'],
+            r['count'],
+            r['envelope_to'],
+            r['envelope_from'],
+            r['header_from'],
+            r['dkim_domain'],
+            r['dkim_result'],
+            r['spf_result'],
+            r['disposition'],
+        ]
+        for r in records
+    ]
 
     print("\n DMARC Report Summary:\n")
-    print(tabulate(row, headers=headers, tablefmt="grid"))
-
+    print(tabulate(rows, headers=headers, tablefmt="fancy_grid"))
